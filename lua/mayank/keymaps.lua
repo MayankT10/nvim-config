e8fac9db32b1f315d72009ea3d68c2294c234da6
MayankT10 <170873949+MayankT10@users.noreply.github.com>
2025-12-17 02:37:03 +0530

    Altering keymaps and adding keymaps for tabby

--- lua/mayank/keymaps.lua
+++ lua/mayank/keymaps.lua
@@ -11,21 +11,21 @@ vim.g.mapleader = " "
 -- end, { desc = 'Format with LSP' })
 
 -- Code runner
-vim.keymap.set('n', '<leader>rr', ':RunCode<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>rr", ":RunCode<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>rf", ":RunFile<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>rft", ":RunFile tab<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>rp", ":RunProject<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>rc", ":RunClose<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>crf", ":CRFiletype<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>crp", ":CRProjects<CR>", { noremap = true, silent = false })
 
 -- conform powered formatting
 vim.keymap.set("n", "<leader>m", function()
-	require("conform").format({ async = true, lsp_fallback = true })
+    require("conform").format({ async = true, lsp_fallback = true })
 end, { desc = "Format file (conform)" })
 
 vim.keymap.set("n", "<leader>j", function()
-	require("conform").format({ async = true, lsp_fallback = true })
+    require("conform").format({ async = true, lsp_fallback = true })
 end, { desc = "Format file (conform)" })
 
 -- Format C++ files using clangd
@@ -55,21 +55,20 @@ end, { desc = "Format file (conform)" })
 
 -- Competitest
 vim.keymap.set("n", "<leader>ce", function()
-  require("competitest").view_stderr()
+    require("competitest").view_stderr()
 end, { desc = "View Competitest stderr output" })
 
 vim.keymap.set("n", "<leader>co", "<cmd>CompetiTestShowOutput<CR>", { desc = "Show last output" })
 
-
 -- Auto-start clangd for C/C++ files
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp", "h", "hpp" },
-  callback = function()
-    local server = vim.lsp.config["clangd"]
-    if server then
-      vim.lsp.start(server)
-    end
-  end,
+    pattern = { "c", "cpp", "h", "hpp" },
+    callback = function()
+        local server = vim.lsp.config["clangd"]
+        if server then
+            vim.lsp.start(server)
+        end
+    end,
 })
 -- vim.keymap.set("n", "<leader>M", "VGgg=")
 
@@ -114,7 +113,7 @@ vim.keymap.set("n", "<C-p>", ":tabprevious<CR>", opts)
 vim.keymap.set("n", "<C-n>", ":tabnext<CR>", opts)
 
 -- Floating terminal
-vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle floating terminal" })
+vim.keymap.set("n", "<leader>T", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle floating terminal" })
 vim.keymap.set("n", "<C-`>", '<cmd>execute v:count . "ToggleTerm"<CR>')
 -- Exit terminal mode
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
@@ -125,3 +124,14 @@ vim.keymap.set("n", "<F3>", "<cmd>CompetiTest edit_testcase<CR>", { desc = "Edit
 vim.keymap.set("n", "<F4>", "<cmd>CompetiTest delete_testcase<CR>", { desc = "Delete Testcases" })
 vim.keymap.set("n", "<F5>", "<cmd>CompetiTest run<CR>", { desc = "Compile and Run" })
 vim.keymap.set("n", "<F6>", "<cmd>CompetiTest run_no_compile<CR>", { desc = "Run without Compiling" })
+
+-- tabby keymaps
+vim.api.nvim_set_keymap("n", "<leader>ta", ":$tabnew<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tc", ":tabclose<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>to", ":tabonly<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tn", ":tabn<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tp", ":tabp<CR>", { noremap = true })
+-- move current tab to previous position
+vim.api.nvim_set_keymap("n", "<leader>tmp", ":-tabmove<CR>", { noremap = true })
+-- move current tab to next position
+vim.api.nvim_set_keymap("n", "<leader>tmn", ":+tabmove<CR>", { noremap = true })
